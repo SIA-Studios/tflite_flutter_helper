b1994cc16593b9542658f93d3a565c886ae734f1
Finn <dereinefinn@gmail.com>
2023-05-30 23:08:00 +0200

    fix image conversion issue remove example

--- lib/src/image/image_conversions.dart
+++ lib/src/image/image_conversions.dart
@@ -23,7 +23,7 @@ class ImageConversions {
       int r = rgbValues[j++];
       int g = rgbValues[j++];
       int b = rgbValues[j++];
-      image.setPixelRgba(wi, hi, r, g, b, 1);
+      image.setPixelRgb(wi, hi, r, g, b);
       wi++;
       if (wi % w == 0) {
         wi = 0;
@@ -44,34 +44,60 @@ class ImageConversions {
     final grayscale = ColorSpaceType.GRAYSCALE;
     grayscale.assertShape(shape);
 
-    final image = Image.fromBytes(width: grayscale.getWidth(shape),
-        height: grayscale.getHeight(shape), bytes: uint8Buffer.getBuffer());
+    final image = Image.fromBytes(
+        width: grayscale.getWidth(shape),
+        height: grayscale.getHeight(shape),
+        bytes: uint8Buffer.getBuffer(),//.asUint8List(),
+        format: Format.uint8);
 
     return image;
   }
 
+  // image is 256 x 256 x 3 channels = 196608 bytes
+  // buffer is also 196608 bytes long
+  // inValues is also 196608 long
   static void convertImageToTensorBuffer(Image image, TensorBuffer buffer) {
     int w = image.width;
     int h = image.height;
-    List<int> intValues = image.data!.getBytes(order: ChannelOrder.abgr);
     int flatSize = w * h * 3;
     List<int> shape = [h, w, 3];
+    var intValues = image.data?.toUint8List();
+
+    // buffer.loadBuffer(image.data!.buffer!, shape: shape);
+
+
     switch (buffer.getDataType()) {
       case TfLiteType.uint8:
         List<int> byteArr = List.filled(flatSize, 0);
-        for (int i = 0, j = 0; i < intValues.length; i++) {
-          byteArr[j++] = ((intValues[i]) & 0xFF);
-          byteArr[j++] = ((intValues[i] >> 8) & 0xFF);
-          byteArr[j++] = ((intValues[i] >> 16) & 0xFF);
+        for (int i = 0, j = 0; i < intValues!.length; i += 3) {
+          byteArr[j++] = (intValues[i] & 0xFF);
+          byteArr[j++] = ((intValues[i + 1] >> 8) & 0xFF);
+          byteArr[j++] = ((intValues[i + 2] >> 16) & 0xFF);
+
+          // final int r = (intValues[i] & 0xFF);
+          // final int g = ((intValues[i] >> 8) & 0xFF);
+          // final int b = ((intValues[i] >> 16) & 0xFF);
+
+          // buffer.byteData.setInt8(i, r);
+          // buffer.byteData.setInt8(i + 1, g);
+          // buffer.byteData.setInt8(i + 2, b);
         }
         buffer.loadList(byteArr, shape: shape);
         break;
       case TfLiteType.float32:
         List<double> floatArr = List.filled(flatSize, 0.0);
-        for (int i = 0, j = 0; i < intValues.length; i++) {
+        for (int i = 0, j = 0; i < intValues!.length; i += 3) {
           floatArr[j++] = ((intValues[i]) & 0xFF).toDouble();
-          floatArr[j++] = ((intValues[i] >> 8) & 0xFF).toDouble();
-          floatArr[j++] = ((intValues[i] >> 16) & 0xFF).toDouble();
+          floatArr[j++] = ((intValues[i + 1] >> 8) & 0xFF).toDouble();
+          floatArr[j++] = ((intValues[i + 2] >> 16) & 0xFF).toDouble();
+
+          // final double r = (intValues[i] & 0xFF).toDouble();
+          // final double g = ((intValues[i] >> 8) & 0xFF).toDouble();
+          // final double b = ((intValues[i] >> 16) & 0xFF).toDouble();
+          //
+          // buffer.byteData.setFloat32(i, r);
+          // buffer.byteData.setFloat32(i + 1, g);
+          // buffer.byteData.setFloat32(i + 2, b);
         }
         buffer.loadList(floatArr, shape: shape);
         break;
